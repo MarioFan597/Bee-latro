450800dfd806ba589f3a06ff75c0a9de3a0f66f7
Hugh Janus <email@example.com>
2025-03-23 17:03:51 -0500

    Minor naming fix for Apes in Tunica (updated the amount of bees in the name)

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -192,7 +192,7 @@ return {
 		    }
         },
         j_bee_trenchcoat = {
-        	name = 'MMMMCXXIX Apes in Tunica',
+        	name = 'MMMMCCXC Apes in Tunica',
         	text = {
 			    "This counts as",
                 "{C:attention}#1# Bees{}",
